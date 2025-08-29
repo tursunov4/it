6c79b94fa4cfcdebd4f1210c3268a4017c4588e4
Sherzodjon <124202641+tursunov4@users.noreply.github.com>
2025-08-29 20:05:42 +0500

    dasd

--- app/service/[slug]/page.tsx
+++ app/service/[slug]/page.tsx
@@ -5,19 +5,20 @@ import ServiceItemHeroSection from "@/components/sections/ServiceItemHeroSection
 import ServicesItemAbout from "@/components/sections/ServicesItemAbout";
 import ServicesItemSection from "@/components/sections/ServicesItemSection";
 import { ServiceType } from "@/types";
+import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
-const LandingPage = ({ params }: { params: { slug: string } }) => {
+const LandingPage = () => {
   const [data, setData] = useState<ServiceType | null>(null);
-
+  const { slug } = useParams<{ slug: string }>();
   useEffect(() => {
     api
-      .get(`/uslugi/${params.slug}/`)
+      .get(`/uslugi/${slug}/`)
       .then((res) => {
         setData(res.data);
       })
       .catch((err) => console.log(err));
-  }, [params.slug]);
+  }, [slug]);
   return (
     <div className=" pb-[48px] md:pb-[64px]">
       <ServiceItemHeroSection data={data} />
